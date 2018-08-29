e11413952a426462b452ed58598f52c80955dc07
Isi9898 <40384074+Isi9898@users.noreply.github.com>
2018-08-29 14:48:14 +0200

    change documentation

--- schroedinger.py
+++ schroedinger.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 24 16:39:23 2018
-
-@author: isi
+Executable file which contains solver and visualizer
 """
 
 import schroed_solver
 import schroed_visualize
 
+# enter the directory of the problem
 direc = "inputdata/asym_potential_well"
+
+# set options for a better illustration of the solutions
 bulge_factor = 0.2
 lim_x = 0.5
 lim_y = 0.2
 
+# execute solver and visualizer
 schroed_solver.solver(direc)
 schroed_visualize.visualize(direc, bulge_factor, lim_x, lim_y)
