fa0bf029ceda8128c6cf2ea98d7a760690dcc3a1
willmajo <40384047+willmajo@users.noreply.github.com>
2018-08-30 22:42:55 +0200

    Add executable visualizer

---
+++ visualize.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Executable file which contains solver and visualizer"""
+
+import argparse
+from schroed_visualize import visualize
+
+
+parser = argparse.ArgumentParser(description='Visualizes solution of the\
+                                 solver')
+
+msg_direc = 'Set directory of the figures\
+    (default value: .)'
+parser.add_argument('-d', '--directory', default='.',
+                    help=msg_direc)
+
+msg_bulge_factor = 'Set bulge factor for the wavefuncs\
+                    (default value: 0.3)'
+parser.add_argument('-bf', '--bulge_factor', default=0.3, type=float,
+                    help=msg_bulge_factor)
+
+msg_lim_x = 'Set x limits of the figures\
+                    (default value: 0.2)'
+parser.add_argument('-xl', '--lim_x', default=0.2, type=float,
+                    help=msg_lim_x)
+
+msg_lim_y = 'Set y limits of the figures\
+                    (default value: 0.5)'
+parser.add_argument('-yl', '--lim_y', default=0.5, type=float,
+                    help=msg_lim_y)
+
+args = parser.parse_args()
+print("Directory: {}".format(args.directory))
+print("bulge factor: {:f}".format(args.bulge_factor))
+print("x limit: {:f}".format(args.lim_x))
+print("y limit: {:f}".format(args.lim_y))
+
+visualize(args.directory, args.bulge_factor, args.lim_x, args.lim_y)
