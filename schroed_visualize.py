6d3e32ad4b90bcd1f38d289212081c22bcd5650b
Isi9898 <40384074+Isi9898@users.noreply.github.com>
2018-08-30 19:07:42 +0200

    define private function

--- schroed_visualize.py
+++ schroed_visualize.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def visualize(direc, bulge_factor, lim_x, lim_y):
+def _visualize(direc, bulge_factor, lim_x, lim_y):
     """Read data from outputfiles and create plots for potential, eigenvalues,
     wavefunctions, expactation values and uncertainty for the position.
 
@@ -22,45 +22,60 @@ def visualize(direc, bulge_factor, lim_x, lim_y):
     # load data files
     xx = np.loadtxt(os.path.join(direc, "potential.dat"))[:, 0]
     potential = np.loadtxt(os.path.join(direc, "potential.dat"))[:, 1]
-    energies = np.loadtxt(os.path.join(direc, "energies.dat"))
+    energy = np.loadtxt(os.path.join(direc, "energies.dat"))
     wavefuncs = np.loadtxt(os.path.join(direc, "wavefuncs.dat"))[:, 1:]
     expvalues = np.loadtxt(os.path.join(direc, "expvalues.dat"))[:, 0]
     uncertainty = np.loadtxt(os.path.join(direc, "expvalues.dat"))[:, 1]
 
-    # plotting potential, energies, wafefunctions and expactationvalues
+    # plotting potential, energies, wafefunctions and expactation values
     plt.subplot(1, 2, 1)
     plt.plot(xx, potential, color="black")
 
     bf = bulge_factor
     limx = lim_x
     limy = lim_y
-    bb = len(energies)
-    for ii in range(0, bb):
-        energy = np.linspace(energies[ii], energies[ii], len(xx))
+    for ii in range(0, int(len(energy))):
+        energy = np.linspace(energy[ii], energy[ii], len(xx))
         plt.plot(xx, energy, color="gray")
-        plt.plot(expvalues, energies, "xg")
+        plt.plot(expvalues, energy, "xg")
         if ii % 2 == 0:
-            plt.plot(xx, bf*wavefuncs[:, ii]+energies[ii], color="blue")
+            plt.plot(xx, bf*wavefuncs[:, ii]+energy[ii], color="blue")
         else:
-            plt.plot(xx, bf*wavefuncs[:, ii]+energies[ii], color="red")
+            plt.plot(xx, bf*wavefuncs[:, ii]+energy[ii], color="red")
     plt.xlim(xx.min() - limx, xx.max() + limx)
-    plt.ylim(potential.min() - limy, energies.max() + limy)
+    plt.ylim(potential.min() - limy, energy.max() + limy)
     plt.xlabel("$x$ [Bohr]")
     plt.ylabel("Energy [Hartree]")
     plt.title("Potential, eigenstates, ($x$)")
 
     # plotting energies and uncertainty
     plt.subplot(1, 2, 2)
-    bb = len(energies)
-    for ii in range(0, bb):
-        energy = np.linspace(energies[ii], energies[ii], len(xx))
+    for ii in range(0, int(len(energy))):
+        energy = np.linspace(energy[ii], energy[ii], len(xx))
         plt.plot(xx, energy, color="gray")
-        plt.plot(uncertainty, energies, "+m")
+        plt.plot(uncertainty, energy, "+m")
         plt.yticks([])
     plt.xlim(0, uncertainty.max() + limx)
-    plt.ylim(potential.min() - limy, energies.max() + limy)
+    plt.ylim(potential.min() - limy, energy.max() + limy)
     plt.xlabel("[Bohr]")
     plt.title("$\sigma_\mathrm{x}$")
 
+    # save figure as solution_schroedinger.pdf
     fname = os.path.join(direc, 'solution_schoedinger.pdf')
     plt.savefig(fname, format="pdf")
+
+
+def visualize(direc, bulge_factor, lim_x, lim_y):
+    """Read data from outputfiles and create plots for potential, eigenvalues,
+    wavefunctions, expactation values and uncertainty for the position.
+
+    Args:
+        direc: directory of the outputfiles
+        bulge_factor: bulge amplitude of the wavefunctions
+        lim_x: set the limits for the x-axis with additional summand
+        lim_y: set the limits for the y-axis with additional summand
+
+    Returns:
+    """
+    # private visualize function will be executed
+    _visualize(direc, bulge_factor, lim_x, lim_y)
